462077a6c90981d82159484d37a357ec6781d535
TakayukiKiyohara <nezumimusume30@gmail.com>
2018-08-17 01:27:34 +0900

    Chapter12まで作成

---
+++ Chapter_11_answer/Game/Star.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "Star.h"
+
+
+Star::Star()
+{
+	m_skinModelRender = NewGO<prefab::CSkinModelRender>(0);
+	m_skinModelRender->Init(L"modelData/star/star.cmo");
+}
+
+
+Star::~Star()
+{
+}
+void Star::Update()
+{
+	//Star‚ÉÀ•W‚ð“`‚¦‚éB
+	m_skinModelRender->SetPosition(m_position);
+}
---
+++ Chapter_11_answer/Game/Star.h
@@ -0,0 +1,11 @@
+#pragma once
+class Star : public IGameObject
+{
+public:
+	Star();
+	~Star();
+	void Update() ;
+	prefab::CSkinModelRender* m_skinModelRender = nullptr;
+	CVector3 m_position = CVector3::Zero;
+};
+
